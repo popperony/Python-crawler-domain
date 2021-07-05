1740e434854605db10e26de6a89279851c29f379
Vadim <vadim_berezhnoy@idpowers.com>
2021-07-05 17:39:26 +0300

    add js parser

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import argparse
 from urllib.parse import urlparse
 import link
-# import link_js
+import link_js
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="The URL to extract links from.")
-    parser.add_argument("-m", "--max-urls", help="Number of max URLs to crawl, default is 10.", default=10, type=int)
+    parser.add_argument("-m", "--max-urls", help="Number of max URLs to crawl, default is 10.", default=5, type=int)
 
     args = parser.parse_args()
     url = args.url
@@ -15,9 +15,9 @@ if __name__ == '__main__':
     # сбор ссылок с сайта
     all_urls = link.crawl(url, max_urls=max_urls)
     # сбор ссылок с сайта которые используются javascript
-    # all_urls_js = link_js.crawl(url, max_urls=max_urls)
+    all_urls_js = link_js.crawl(url, max_urls=max_urls)
     # объединение в один массив
-    # all_urls = all_urls.union(all_urls_js)
+    all_urls = all_urls.union(all_urls_js)
     # доменное имя для названия файла
     domain_name = urlparse(url).netloc
 
@@ -27,7 +27,11 @@ if __name__ == '__main__':
         for internal_link in all_urls:
             print(internal_link.strip(), file=f)
 
-    print("[+] Total Internal links:", len(link.internal_urls))
-    print("[+] Total External links:", len(link.external_urls))
-    print("[+] Total URLs:", len(link.external_urls) + len(link.internal_urls))
+    print("[+] General Internal links:", len(link.internal_urls))
+    print("[+] General External links:", len(link.external_urls))
+    print("[+] Javascript Internal links:", len(link_js.internal_urls_js))
+    print("[+] Javascript External links:", len(link_js.external_urls_js))
+    print("[+] Total Internal links:", len(link.internal_urls) + len(link_js.internal_urls_js))
+    print("[+] Total External links:", len(link.external_urls) + len(link_js.external_urls_js))
+    print("[+] Total URLs:", len(link.external_urls) + len(link.internal_urls) + len(link_js.internal_urls_js) + len(link_js.external_urls_js))
     print("[+] Total crawled URLs:", max_urls)
