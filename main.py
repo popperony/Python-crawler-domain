363be457e0620d38291d37aab6f140f3e8638f98
Вадим <popperony@yandex.ru>
2021-07-05 19:40:37 +0300

    PEP8 and win10 adaptation

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 import argparse
 from urllib.parse import urlparse
-import link
-import link_js
+
+import parser_link
+import parser_link_js
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="The URL to extract links from.")
-    parser.add_argument("-m", "--max-urls", help="Number of max URLs to crawl, default is 10.", default=5, type=int)
+    parser.add_argument("-m", "--max-urls", help="Number of max URLs to crawl, default is 10.", default=5, type=int)  # NOQA E501
 
     args = parser.parse_args()
     url = args.url
     max_urls = args.max_urls
     # сбор ссылок с сайта
-    all_urls = link.crawl(url, max_urls=max_urls)
+    all_urls = parser_link.crawl(url, max_urls=max_urls)
     # сбор ссылок с сайта которые используются javascript
-    all_urls_js = link_js.crawl(url, max_urls=max_urls)
+    all_urls_js = parser_link_js.crawl(url, max_urls=max_urls)
     # объединение в один массив
     all_urls = all_urls.union(all_urls_js)
     # доменное имя для названия файла
     domain_name = urlparse(url).netloc
 
+    with open(f"output/{domain_name}_links.txt", "w", encoding='utf-8') as file:  # NOQA E501
+        for i in all_urls:
+            print(i.strip(), file=file)
 
-
-    with open(f"output/{domain_name}_links.txt", "w") as f:
-        for internal_link in all_urls:
-            print(internal_link.strip(), file=f)
-
-    print("[+] General Internal links:", len(link.internal_urls))
-    print("[+] General External links:", len(link.external_urls))
-    print("[+] Javascript Internal links:", len(link_js.internal_urls_js))
-    print("[+] Javascript External links:", len(link_js.external_urls_js))
-    print("[+] Total Internal links:", len(link.internal_urls) + len(link_js.internal_urls_js))
-    print("[+] Total External links:", len(link.external_urls) + len(link_js.external_urls_js))
-    print("[+] Total URLs:", len(link.external_urls) + len(link.internal_urls) + len(link_js.internal_urls_js) + len(link_js.external_urls_js))
+    # print("[+] General Internal links:", len(parser_link.internal_urls))
+    # print("[+] General External links:", len(parser_link.external_urls))
+    # print("[+] Javascript Internal links:", len(parser_link_js.internal_urls_js))  # NOQA E501
+    # print("[+] Javascript External links:", len(parser_link_js.external_urls_js))  # NOQA E501
+    print("[+] Total Internal links:", len(parser_link.internal_urls) + len(parser_link_js.internal_urls_js))  # NOQA E501
+    print("[+] Total External links:", len(parser_link.external_urls) + len(parser_link_js.external_urls_js))  # NOQA E501
+    print("[+] Total URLs:", len(parser_link.external_urls) + len(parser_link.internal_urls) + len(parser_link_js.internal_urls_js) + len(parser_link_js.external_urls_js))  # NOQA E501
     print("[+] Total crawled URLs:", max_urls)
---
+++ parser_link.py
@@ -0,0 +1,89 @@
+import requests
+from urllib.request import urlparse, urljoin
+from bs4 import BeautifulSoup
+import colorama
+
+# определяем цвета для внешних и внутренних ссылок
+colorama.init()
+GREEN = colorama.Fore.GREEN
+BLUE = colorama.Fore.BLUE
+RED = colorama.Fore.RED
+RESET = colorama.Fore.RESET
+
+# множество для сбора только уникальных ссылок
+all_urls = set()
+
+total_urls_visited = 0
+# внутренние ссылки - на страницы внутри сайта
+internal_urls = set()
+# внешние ссылки - на другие сайты
+external_urls = set()
+
+
+# проверяем в url правильную схему и домен
+def is_valid(url):
+    parsed = urlparse(url)
+    if bool(parsed.netloc) and bool(parsed.scheme):
+        return True
+    return False
+
+
+def get_all_website_links(url):
+    """
+    Возвращает все найденные URL на текущем сайте
+    """
+    urls = set()
+    # доменное имя без указания протокола
+    domain_name = urlparse(url).netloc
+    try:
+        global soup
+        soup = BeautifulSoup(requests.get(url).content, "html.parser", from_encoding="iso-8859-1")  # NOQA E501
+    except Exception as exc:
+        print(f'{RED} {exc} {RESET}')
+
+    for a_tag in soup.findAll("a"):
+        href = a_tag.attrs.get("href")
+        if href == "" or href is None:
+            # если в тэге нет ссылки
+            continue
+        # присоединям к URL домен если ссылка относительная
+        href = urljoin(url, href)
+        parsed_href = urlparse(href)
+        # очищаем URL от лишних параметров
+        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path  # NOQA E501
+        if not is_valid(href):
+            # если не валидна ссылка идем к след
+            continue
+        if href in all_urls:
+            # если есть уже в наборе
+            continue
+        if domain_name not in href:
+            # если домена нет в ссылке, значит она внешняя
+            if urlparse(href).netloc not in all_urls:
+                # печатаем серым цветом и сохраняем в набор (наше множество)
+                print(f"{BLUE}[!] External link: {href}{RESET}")
+                all_urls.add(href)
+                external_urls.add(href)
+            continue
+        urls.add(href)
+        if parsed_href.netloc not in all_urls:
+            print(f"{GREEN}[*] Internal link: {href}{RESET}")
+            all_urls.add(href)
+            internal_urls.add(href)
+    return urls
+
+
+def crawl(url, max_urls=5):
+    """
+    Сканирвание страницы, извлечение всех ссылок.
+    Все ссылки складываются в наборы external_urls и internal_urls
+    max_urls - максимальное количество ссылок для сканирования, по умолчанию 5
+    """
+    global total_urls_visited
+    total_urls_visited += 1
+    links = get_all_website_links(url)
+    for link in links:
+        if total_urls_visited > max_urls:
+            break
+        crawl(link, max_urls=max_urls)
+    return all_urls
